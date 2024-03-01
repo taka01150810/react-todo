6638d93a4ba52d314df8dbebdd9abbebc71f24dd
宮中良明 <miyanaka@aidiot.jp>
2024-03-01 11:23:56 +0900

    Reactでのイベントやスタイルの扱い方を知る

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 // exportで他のファイルからも使えるようにする
 export const App = () => {
+  const onclickButton = () => {alert('こんにちは')};
+  const contentStyle = {
+    color: 'blue',
+    fontSize: '18px'
+  }
     return (
       <div>
-        <h1>こんにちは！</h1>
+        {/* オブジェクトでCSSを表現することができる */}
+        <h1 style={{ color: 'red' }}>こんにちは！</h1>
+        <h1 style={ contentStyle }>こんにちは！</h1>
+
         <p>お元気ですか？</p>
+
+        {/* JSの書き方 */}
+        {/* <button onclick={() => {alert('こんにちは')}}>ボタン</button> */}
+
+        {/* 
+        Reactの書き方 
+        {}の中でJSのコードを実行できる
+        */}
+        <button onClick={() => {alert('こんにちは')}}>ボタン</button>
+        { console.log('こんにちは') }
+        <button onClick={onclickButton}>ボタン</button>
       </div>
     );
 };
