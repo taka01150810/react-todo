b0c95ea4c362b89e888cf566cd715f91b7c49f37
宮中良明 <miyanaka@aidiot.jp>
2024-03-04 12:36:00 +0900

    コード整形

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,12 +3,12 @@ import "./styles.css";
 export const Todo = () => {
   return (
     <>
-    <div className="input-area">
-      <input placeholder="Todoを入力" />
-      <button>追加</button>
-    </div>
-    <div className="incomplete-area">
-      <p className="title">未完了のTodo</p>
+      <div className="input-area">
+        <input placeholder="Todoを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTodo</p>
         <ul>
           <div className="list-row">
             <li>TODOです</li>
@@ -16,16 +16,16 @@ export const Todo = () => {
             <button>削除</button>
           </div>
         </ul>
-    </div>
-    <div className="complete-area">
-      <p className="title">完了のTodo</p>
-      <ul>
-        <div className="list-row">
-          <li>TODOでした</li>
-          <button>戻す</button>
-        </div>
-      </ul>
-    </div>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTodo</p>
+        <ul>
+          <div className="list-row">
+            <li>TODOでした</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
     </>
   );
-}
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import { Todo } from './Todo'
+import { Todo } from "./Todo";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
